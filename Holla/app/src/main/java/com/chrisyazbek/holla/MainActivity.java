bce364286c2a13c763b404799010579077eae505
chris yazbek <christopher.yazbek@gmail.com>
2017-02-04 23:31:59 -0500

    Reza is back

--- Holla/app/src/main/java/com/chrisyazbek/holla/MainActivity.java
+++ Holla/app/src/main/java/com/chrisyazbek/holla/MainActivity.java
@@ -3,8 +3,12 @@ package com.chrisyazbek.holla;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        contactButton = (Button) findViewById(R.id.contactButton);
+        /*contactButton = (Button) findViewById(R.id.contactButton);
         contactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ContactActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        favButton = (Button) findViewById(R.id.favButton);
-        favButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, FavActivity.class);
                 startActivity(intent);
             }
         });
@@ -42,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        */
+
+        ImageView start = (ImageView) findViewById(R.id.startButtonView);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FavActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
