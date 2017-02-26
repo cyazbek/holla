9d99c1483e630b05960b7e62b06e23924b8557fc
Reza Madabadi <rezmaza@gmail.com>
2017-02-25 23:05:47 -0500

    Prettified code with ButterKnife

--- Holla/app/src/main/java/com/chrisyazbek/holla/DoneActivity.java
+++ Holla/app/src/main/java/com/chrisyazbek/holla/DoneActivity.java
@@ -8,32 +8,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class DoneActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_done);
-
-
-        Button doneB = (Button) findViewById(R.id.hollazButtonDone);
-        doneB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(DoneActivity.this, HollaAtActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button anotherB = (Button) findViewById(R.id.anotherHolla);
-        anotherB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(DoneActivity.this, FavActivity.class);
-                startActivity(intent);
-            }
-        });
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.hollazButtonDone)
+    public void GoToQueueActivity()
+    {
+        Intent intent = new Intent(DoneActivity.this, HollaAtActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.anotherHolla)
+    public void GoBack()
+    {
+        Intent intent = new Intent(DoneActivity.this, FavActivity.class);
+        startActivity(intent);
+    }
+
 }
